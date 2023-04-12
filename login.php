a1273cba82e38c33dc5a99f5759b20a9b1fa717b
Tawhid Mirza Mahib <dhakaiyacoder@gmail.com>
2023-04-12 17:12:49 +0600

    Registration alert,bug,backlog fixed

--- login.php
+++ login.php
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="login.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="login.css"/>
     <title>Login</title>
 </head>
 <body>
-  <div class="container">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  <div class="login_container">
     <div class="side_image">
       <img src="images/register_pic.jpg" alt="Side Image" class="side_pic" />
     </div>
@@ -20,7 +22,7 @@
           <div class="user_detail">
             
             <div class="input_box">
-              <span class="detail">Email:</span>
+              <span class="detail">Username:</span>
               <input
                 type="text"
                 name = "username" 
@@ -45,12 +47,12 @@
                 <lebel for="check">Remember me</lebel>
               </div>
             <div class="right">
-              <lebel><a href="#" style="text-decoration: none"class="link">Forgot password</a></lebel>
+              <lebel><a href="#" style="text-decoration: none"class="link">Forgot password?</a></lebel>
             </div>
           </div>
           
-          <div class="button">
-            <input type="submit" value="Sign Up" />
+          <div>
+            <input class="signin_button" type="submit" value="Sign In" />
           </div>
         </form>
       </div>
